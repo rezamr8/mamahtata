e2ed0e2fe64058a260b809ee3f9bffaa5b0132f2
edzapodka <edzapodka@gmail.com>
2018-04-06 21:53:58 +0700

    action bayar lunas khusus admin

--- resources/views/layouts/action.blade.php
+++ resources/views/layouts/action.blade.php
@@ -8,7 +8,11 @@
 			<a href="/admin/orders/{{$id}}/edit" class="btn btn-xs btn-primary"><i class="fa fa-pencil-square-o"></i> Edit</a>
 			@endif
 		@else
+		    @if($piutang > 0)
 			<a href="/admin/orders/{{$id}}/bayar" class="btn btn-xs btn-info"><i class="fa fa-money"></i> Bayar</a>
+			@else
+				<i class="fa fa-check btn btn-info"></i>
+			@endif
 		@endhasrole
 
 @endhasrole
